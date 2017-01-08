038351f6b5984c39dab34c08e99f5b3f8dffdd4e
anan17-meet <anan17@meet.mit.edu||>
2017-01-08 19:56:56 +0200

    week10

--- webapp.py
+++ webapp.py
@@ -1,21 +1,57 @@
 from flask import Flask
+from flask import session as login_session
 from model import *
 
 app = Flask(__name__)
-
 engine = create_engine('sqlite:///fizzBuzz.db')
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine, autoflush=False)
 session = DBSession()
 
 @app.route('/')
+def showInventory():
+	inventory = session.query(Product).all()
+	htmlString = ""
+	for item in inventory:
+		htmlString += "<p>" + item.name + "</p>" + "<p>" + item.description + "</p>" + "<p>" + item.price + "</br></br>" 
+	return htmlString
+
+def hello_world():
+	return 'hello world!'
+	
+@app.route('/')
 @app.route('/inventory')
 def inventory():
-	return "To be implemented"
+	item = session.query(Product).all()
+	return render_template("inventory.html", item = items)
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
-	return "To be implmented"
+	if request.method == 'GET':
+		return render_template('login.html')
+	elif request.method == 'POST':
+		email = request.form['email']
+		password = request.form['password']
+		if email is None or password is None:
+			flash('Missing Arguments')
+			redirect(url_for('login'))
+	if verify_password(email, password):
+		customer = session.query(Customer).filter_by(email=email).one()
+		flash('login Successful, welcome, %s' % customer.name)
+		login_session['name'] = customer.name
+		login_session['email'] = email
+		login_session['id'] = customer.id
+		return redirect(url_for('inventory'))
+	else:
+		flash('Incorrect username/password combination')
+		return redirect(url_for('login'))
+
+def verify_password(email, password):
+	customer = session.query(Customer).filter_by(email = email).first()
+	if not customer or not customer.verify_password(password):
+		return False
+	g.customer = customer
+	return True	
 
 @app.route('/newCustomer', methods = ['GET','POST'])
 def newCustomer():
@@ -23,7 +59,8 @@ def newCustomer():
 
 @app.route("/product/<int:product_id>")
 def product(product_id):
-	return "To be implemented"
+	product = session.query(Product).filter_by(id=product_id).one()
+	return render_template('product.html', product=product)
 
 @app.route("/product/<int:product_id>/addToCart", methods = ['POST'])
 def addToCart(product_id):
@@ -54,4 +91,4 @@ def logout():
 	return "To be implmented"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
